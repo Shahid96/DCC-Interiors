a77c9b4e9cd51e93e2a74fd3e97e7937bc661a99
Shahid96 <razashahid6@gmail.com>
2018-01-15 22:17:47 +0530

    First Commit

---
+++ js/dcc.js
@@ -0,0 +1,8 @@
+ $('.carousel.carousel-slider').carousel({fullWidth: true});
+   setInterval(function() {
+    $('.carousel').carousel('next');
+  }, 3700); 
+
+    $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
